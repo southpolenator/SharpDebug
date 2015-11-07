a7449a0b4bb36c3bcef2bb7c1b04e14c3ba0ef92
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-07 23:02:43 +0100

    Adding CsScript project for WinDbg extension wrapper

---
+++ CsScripts/CsScripts.cpp
@@ -0,0 +1,11 @@
+// CsScripts.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "CsScripts.h"
+
+// This is an example of an exported function.
+CSSCRIPTS_API int fnCsScripts(void)
+{
+    return 42;
+}
---
+++ CsScripts/CsScripts.h
@@ -0,0 +1,17 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the CSSCRIPTS_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// CSSCRIPTS_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef CSSCRIPTS_EXPORTS
+#define CSSCRIPTS_API __declspec(dllexport)
+#else
+#define CSSCRIPTS_API __declspec(dllimport)
+#endif
+
+extern "C" {
+
+CSSCRIPTS_API int fnCsScripts(void);
+
+};
