78815348b91628c1299fc8dac71c604ce33ac549
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-14 13:19:50 +0100

    Adding comments to Constants

--- CsScriptManaged/Constants.cs
+++ CsScriptManaged/Constants.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CsScriptManaged
+﻿namespace CsScriptManaged
 {
+    /// <summary>
+    /// All constants used in the project
+    /// </summary>
     public class Constants
     {
+        /// <summary>
+        /// The maximum file name
+        /// </summary>
         public const int MaxFileName = 65536;
+
+        /// <summary>
+        /// The maximum symbol name
+        /// </summary>
         public const int MaxSymbolName = 4096;
+        
+        /// <summary>
+        /// The maximum base type identifier
+        /// </summary>
         public const uint MaxBaseTypeId = 2000000000;
+
+        /// <summary>
+        /// The maximum string read length
+        /// </summary>
         public const uint MaxStringReadLength = 10240;
     }
 }
