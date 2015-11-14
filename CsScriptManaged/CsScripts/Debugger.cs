a1571541ac0f3378361b4a0c3b6837f394c8ea6d
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-14 11:46:34 +0100

    Adding comments to Debugger class

--- CsScriptManaged/CsScripts/Debugger.cs
+++ CsScriptManaged/CsScripts/Debugger.cs
@@ -3,8 +3,17 @@ using DbgEngManaged;
 
 namespace CsScripts
 {
+    /// <summary>
+    /// Helper class that controls the debugger
+    /// </summary>
     public class Debugger
     {
+        /// <summary>
+        /// Executes the specified command and captures its output.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns></returns>
         public static string ExecuteAndCapture(string command, params object[] parameters)
         {
             var callbacks = new DebuggerOutputSaver();
@@ -15,6 +24,11 @@ namespace CsScripts
             }
         }
 
+        /// <summary>
+        /// Executes the specified command, but leaves its output visible to the user.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="parameters">The parameters.</param>
         public static void Execute(string command, params object[] parameters)
         {
             command = string.Join(" ", command, string.Join(" ", parameters));
