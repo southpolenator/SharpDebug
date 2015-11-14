235a8d733e0020ca68c95801d5fd18e79527e2b3
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-14 13:33:47 +0100

    Adding comments MarshalStructure

--- CsScriptManaged/MarshalStructure.cs
+++ CsScriptManaged/MarshalStructure.cs
@@ -1,24 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsScriptManaged
 {
+    /// <summary>
+    /// Marshaled structure
+    /// </summary>
+    /// <typeparam name="T">The structure type</typeparam>
     public class MarshalStructure<T> : IDisposable
         where T : struct
     {
+        /// <summary>
+        /// The base size of the structure
+        /// </summary>
         public static readonly int BaseSize = Marshal.SizeOf<T>();
+
+        /// <summary>
+        /// The extended size - more bytes to be allocated after structure
+        /// </summary>
         private int extendedSize;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MarshalStructure{T}"/> class.
+        /// </summary>
+        /// <param name="extended">The extended size.</param>
         public MarshalStructure(int extended = 0)
         {
             ExtendedSize = extended;
             Pointer = Marshal.AllocHGlobal(Size);
         }
 
+        /// <summary>
+        /// Gets or sets the extended size.
+        /// </summary>
         public int ExtendedSize
         {
             get
@@ -39,6 +53,9 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Gets the size.
+        /// </summary>
         public int Size
         {
             get
@@ -47,6 +64,9 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Gets the unsigned size.
+        /// </summary>
         public uint USize
         {
             get
@@ -55,6 +75,9 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Gets or sets the structure.
+        /// </summary>
         public T Structure
         {
             get
@@ -68,6 +91,9 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Gets or sets the extended pointer.
+        /// </summary>
         public IntPtr ExtendedPointer
         {
             get
@@ -81,17 +107,30 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Gets the pointer.
+        /// </summary>
         public IntPtr Pointer { get; private set; }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             Marshal.DestroyStructure<T>(Pointer);
         }
     }
 
+    /// <summary>
+    /// Marshaled structure followed by ANSI string
+    /// </summary>
+    /// <typeparam name="T">The structure type</typeparam>
     public class MarshalStructureExtendedWithAnsiString<T> : MarshalStructure<T>
         where T : struct
     {
+        /// <summary>
+        /// Gets or sets the extended value.
+        /// </summary>
         public string Extended
         {
             get
