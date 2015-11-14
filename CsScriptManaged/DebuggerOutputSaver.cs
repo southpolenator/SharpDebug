b862bc9ba360ccf1be267bb25feb9b72421dfe90
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-14 13:28:19 +0100

    Adding comments to DebuggerOutputSaver

--- CsScriptManaged/DebuggerOutputSaver.cs
+++ CsScriptManaged/DebuggerOutputSaver.cs
@@ -3,17 +3,33 @@ using System.Text;
 
 namespace CsScriptManaged
 {
+    /// <summary>
+    /// Helper class for capturing debugger output while executing commands
+    /// </summary>
     public class DebuggerOutputSaver : IDebugOutputCallbacksWide
     {
+        /// <summary>
+        /// The string builder
+        /// </summary>
         private StringBuilder sb = new StringBuilder();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebuggerOutputSaver"/> class.
+        /// </summary>
+        /// <param name="captureFlags">The capture flags.</param>
         public DebuggerOutputSaver(DebugOutput captureFlags)
         {
             CaptureFlags = captureFlags;
         }
 
+        /// <summary>
+        /// Gets or sets the capture flags.
+        /// </summary>
         public DebugOutput CaptureFlags { get; set; }
 
+        /// <summary>
+        /// Gets the captured text.
+        /// </summary>
         public string Text
         {
             get
@@ -22,6 +38,11 @@ namespace CsScriptManaged
             }
         }
 
+        /// <summary>
+        /// Captures the specified text.
+        /// </summary>
+        /// <param name="mask">The mask.</param>
+        /// <param name="text">The text.</param>
         public void Output(uint mask, string text)
         {
             if ((mask & (uint)CaptureFlags) != 0)
