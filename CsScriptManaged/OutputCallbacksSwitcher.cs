3696580af76ab4c9126408ed0b5e7bb914f619b7
Vuk Jovanovic <southpolenator@gmail.com>
2015-11-14 13:55:33 +0100

    Adding comments to OputputCallbacksSwitcher

--- CsScriptManaged/OutputCallbacksSwitcher.cs
+++ CsScriptManaged/OutputCallbacksSwitcher.cs
@@ -3,16 +3,29 @@ using System;
 
 namespace CsScriptManaged
 {
+    /// <summary>
+    /// Helper class for replacing debugger output callbacks - used when capturing command output
+    /// </summary>
     public class OutputCallbacksSwitcher : IDisposable
     {
+        /// <summary>
+        /// The previous callbacks interface
+        /// </summary>
         private IDebugOutputCallbacksWide previousCallbacks;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OutputCallbacksSwitcher"/> class.
+        /// </summary>
+        /// <param name="newCallbacks">The new callbacks interface.</param>
         public OutputCallbacksSwitcher(IDebugOutputCallbacksWide newCallbacks)
         {
             previousCallbacks = Context.Client.GetOutputCallbacksWide();
             Context.Client.SetOutputCallbacksWide(newCallbacks);
         }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             Context.Client.SetOutputCallbacksWide(previousCallbacks);
