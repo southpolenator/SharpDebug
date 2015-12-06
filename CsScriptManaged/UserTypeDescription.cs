8c89c4d63cc3cf3139b19792834a73e91f2b1a49
southpolenator <southpolenator@gmail.com>
2015-12-06 22:31:30 +0100

    Adding comments in UserTypeDescription

--- CsScriptManaged/UserTypeDescription.cs
+++ CsScriptManaged/UserTypeDescription.cs
@@ -6,6 +6,12 @@ namespace CsScriptManaged
 {
     class UserTypeDescription
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserTypeDescription"/> class.
+        /// </summary>
+        /// <param name="moduleName">Name of the module.</param>
+        /// <param name="typeName">Name of the type.</param>
+        /// <param name="type">The type.</param>
         public UserTypeDescription(string moduleName, string typeName, Type type)
         {
             if (moduleName != null)
@@ -22,6 +28,11 @@ namespace CsScriptManaged
                     throw new Exception(string.Format("Type {0} exists in multiple modules: {1}", typeName, string.Join(", ", modules.Select(m => m.Name))));
                 }
 
+                if (modules.Length <= 0)
+                {
+                    throw new Exception(string.Format("Type {0} wasn't found in any module", typeName));
+                }
+
                 Module = modules[0];
             }
 
@@ -29,10 +40,19 @@ namespace CsScriptManaged
             Type = type;
         }
 
+        /// <summary>
+        /// Gets the module.
+        /// </summary>
         public Module Module { get; private set; }
 
+        /// <summary>
+        /// Gets the user code type.
+        /// </summary>
         public DType UserType { get; private set; }
 
+        /// <summary>
+        /// Gets the user type.
+        /// </summary>
         public Type Type { get; private set; }
     }
 }
