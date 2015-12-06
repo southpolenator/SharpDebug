fb42ba75ae43e57ce63df002f442b3c8e77c2160
southpolenator <southpolenator@gmail.com>
2015-12-06 23:45:29 +0100

    Adding comments to UserTypeMetadata

--- CsScriptManaged/UserTypeMetadata.cs
+++ CsScriptManaged/UserTypeMetadata.cs
@@ -6,6 +6,12 @@ namespace CsScriptManaged
 {
     class UserTypeMetadata
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserTypeMetadata"/> class.
+        /// </summary>
+        /// <param name="moduleName">Name of the module.</param>
+        /// <param name="typeName">Name of the type.</param>
+        /// <param name="type">The type.</param>
         public UserTypeMetadata(string moduleName, string typeName, Type type)
         {
             ModuleName = moduleName;
@@ -13,12 +19,25 @@ namespace CsScriptManaged
             Type = type;
         }
 
+        /// <summary>
+        /// Gets the name of the module.
+        /// </summary>
         public string ModuleName { get; private set; }
 
+        /// <summary>
+        /// Gets the name of the user code type.
+        /// </summary>
         public string TypeName { get; private set; }
 
+        /// <summary>
+        /// Gets the user type.
+        /// </summary>
         public Type Type { get; private set; }
 
+        /// <summary>
+        /// Reads metadata from the type.
+        /// </summary>
+        /// <param name="type">The type.</param>
         public static UserTypeMetadata ReadFromType(Type type)
         {
             UserTypeAttribute attribute = type.GetCustomAttribute<UserTypeAttribute>();
@@ -39,6 +58,9 @@ namespace CsScriptManaged
             return null;
         }
 
+        /// <summary>
+        /// Converts metadata to description.
+        /// </summary>
         public UserTypeDescription ConvertToDescription()
         {
             return new UserTypeDescription(ModuleName, TypeName, Type);
