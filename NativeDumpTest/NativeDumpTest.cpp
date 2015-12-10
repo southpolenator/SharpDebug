f2e0d37d7ad2bc2c96ae88408c3a9c2812c14fdb
southpolenator <southpolenator@gmail.com>
2015-12-10 16:47:05 +0100

    Adding simple native application for generating dump(s) for testing...

---
+++ NativeDumpTest/NativeDumpTest.cpp
@@ -0,0 +1,41 @@
+// NativeDumpTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class MyTestClass
+{
+public:
+	wstring string1;
+	list<wstring> strings;
+	vector<string> ansiStrings;
+
+	static int staticVariable;
+} globalVariable;
+
+int MyTestClass::staticVariable = 1212121212;
+
+void BreakPointFunction()
+{
+}
+
+int main()
+{
+	MyTestClass * p = &globalVariable;
+	MyTestClass ** q = &p;
+
+	p->string1 = L"qwerty";
+	p->strings.push_back(L"Foo");
+	p->strings.push_back(L"Bar");
+	p->ansiStrings.push_back("AnsiFoo");
+	p->ansiStrings.push_back("AnsiBar");
+
+	BreakPointFunction();
+
+    return 0;
+}
