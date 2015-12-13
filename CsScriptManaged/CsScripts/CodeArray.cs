9cc4223b93ad015cd294bd361eec990e5e9ee486
Vuk Jovanovic <southpolenator@gmail.com>
2015-12-13 20:55:01 +0100

    Adding CodeArray for easier traversal of preallocated arrays...

---
+++ CsScriptManaged/CsScripts/CodeArray.cs
@@ -0,0 +1,86 @@
+﻿using CsScripts;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace CsScriptManaged.CsScripts
+{
+    public class CodeArray<T> : IReadOnlyList<T>
+    {
+        /// <summary>
+        /// The actual variable where we get all the values.
+        /// </summary>
+        private Variable variable;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeArray{T}"/> class.
+        /// </summary>
+        /// <param name="variable">The variable.</param>
+        public CodeArray(Variable variable)
+        {
+            if (!variable.GetCodeType().IsArray)
+            {
+                throw new Exception("Wrong code type of passed variable " + variable.GetCodeType().Name);
+            }
+
+            Length = variable.GetArrayLength();
+        }
+
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Gets the number of elements in the collection.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return Length;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="T"/> at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                {
+                    throw new ArgumentOutOfRangeException("index", index, "Index out of array length");
+                }
+
+                return variable.GetArrayElement(index).CastAs<T>();
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Enumerate().GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Enumerate().GetEnumerator();
+        }
+
+        /// <summary>
+        /// Enumerates this array.
+        /// </summary>
+        private IEnumerable<T> Enumerate()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                yield return this[i];
+            }
+        }
+    }
+}
