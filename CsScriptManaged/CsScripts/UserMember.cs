048fb8dd57dbf6e628a864b13984e029236b68ec
vujova@microsoft.com <vujova@microsoft.com>
2016-02-04 00:01:47 +0100

    Adding ToString to UserMember

--- CsScriptManaged/CsScripts/UserMember.cs
+++ CsScriptManaged/CsScripts/UserMember.cs
@@ -84,5 +84,16 @@ namespace CsScripts
                 Cached = true;
             }
         }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return value.ToString();
+        }
     }
 }
