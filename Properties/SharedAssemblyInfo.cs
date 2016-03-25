e57d12a58034288cbe65005241bca4a9a87feacb
vujova@microsoft.com <vujova@microsoft.com>
2016-03-25 12:55:25 +0100

    Changing Copyright message and version number...

--- Properties/SharedAssemblyInfo.cs
+++ Properties/SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © Vuk Jovanovic 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © WinDbgCs team 2015-2016")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -12,5 +12,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
