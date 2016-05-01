191e7feabcf19824360b7e7ba1b67d4e6a64fc71
Vuk Jovanovic <southpolenator@gmail.com>
2016-05-01 23:32:46 +0200

    Fixing comments in CsDebugScript.VS.VSContext

--- Source/CsDebugScript.VS/VSContext.cs
+++ Source/CsDebugScript.VS/VSContext.cs
@@ -3,10 +3,19 @@ using System;
 
 namespace CsDebugScript.VS
 {
+    /// <summary>
+    /// Context for interacting with Visual Studio
+    /// </summary>
     internal static class VSContext
     {
+        /// <summary>
+        /// The debugger events (we need to store this in order to get events)
+        /// </summary>
         private static DebuggerEvents debuggerEvents;
 
+        /// <summary>
+        /// Gets the current debugging mode.
+        /// </summary>
         public static dbgDebugMode CurrentDebugMode
         {
             get
@@ -15,18 +24,40 @@ namespace CsDebugScript.VS
             }
         }
 
+        /// <summary>
+        /// Occurs when debugger enters break mode.
+        /// </summary>
         public static event Action DebuggerEnteredBreakMode;
 
+        /// <summary>
+        /// Occurs when debugger enters design mode.
+        /// </summary>
         public static event Action DebuggerEnteredDesignMode;
 
+        /// <summary>
+        /// Occurs when debugger enters run mode.
+        /// </summary>
         public static event Action DebuggerEnteredRunMode;
 
+        /// <summary>
+        /// Gets the service provider.
+        /// </summary>
         internal static IServiceProvider ServiceProvider { get; private set; }
 
+        /// <summary>
+        /// Gets the DTE.
+        /// </summary>
         internal static DTE DTE { get; private set; }
 
-        internal static VSDebugger VSDebugger;
+        /// <summary>
+        /// The VS debugger
+        /// </summary>
+        internal static VSDebugger VSDebugger { get; private set; }
 
+        /// <summary>
+        /// Sets the service provider.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
         public static void SetServiceProvider(IServiceProvider serviceProvider)
         {
             if (ServiceProvider != serviceProvider)
@@ -41,17 +72,30 @@ namespace CsDebugScript.VS
             }
         }
 
-        private static void DebuggerEvents_OnEnterRunMode(dbgEventReason Reason)
+        /// <summary>
+        /// Occurs when debugger enters run mode.
+        /// </summary>
+        /// <param name="reason">The reason.</param>
+        private static void DebuggerEvents_OnEnterRunMode(dbgEventReason reason)
         {
             DebuggerEnteredRunMode?.Invoke();
         }
 
-        private static void DebuggerEvents_OnEnterDesignMode(dbgEventReason Reason)
+        /// <summary>
+        /// Occurs when debugger enters design mode.
+        /// </summary>
+        /// <param name="reason">The reason.</param>
+        private static void DebuggerEvents_OnEnterDesignMode(dbgEventReason reason)
         {
             DebuggerEnteredDesignMode?.Invoke();
         }
 
-        private static void DebuggerEvents_OnEnterBreakMode(dbgEventReason Reason, ref dbgExecutionAction ExecutionAction)
+        /// <summary>
+        /// Occurs when debugger enters break mode.
+        /// </summary>
+        /// <param name="reason">The reason.</param>
+        /// <param name="executionAction">The execution action.</param>
+        private static void DebuggerEvents_OnEnterBreakMode(dbgEventReason reason, ref dbgExecutionAction executionAction)
         {
             DebuggerEnteredBreakMode?.Invoke();
             // TODO: VSDebugger?.UpdateCache();
