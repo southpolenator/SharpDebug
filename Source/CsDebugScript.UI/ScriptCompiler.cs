26cb974db12e41c70e7863227314af648f940d61
Vuk Jovanovic <southpolenator@gmail.com>
2016-05-24 11:20:54 +0200

    Adding support for System.Dynamic in Roslyn scripts

--- Source/CsDebugScript.UI/ScriptCompiler.cs
+++ Source/CsDebugScript.UI/ScriptCompiler.cs
@@ -53,13 +53,8 @@ namespace CsDebugScript
             assemblyReferences.Add(typeof(CsDebugScript.Variable).Assembly.Location);
             assemblyReferences.Add(typeof(CsDebugScript.InteractiveScriptBase).Assembly.Location);
 
-            // Check if Microsoft.CSharp.dll should be added to the list of referenced assemblies
-            const string MicrosoftCSharpDll = "microsoft.csharp.dll";
-
-            if (!assemblyReferences.Where(a => a.ToLowerInvariant().Contains(MicrosoftCSharpDll)).Any())
-            {
-                assemblyReferences.AddRange(AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic && a.Location.ToLowerInvariant().Contains(MicrosoftCSharpDll)).Select(a => a.Location));
-            }
+            // Add support for System.Dynamic
+            assemblyReferences.Add("Microsoft.CSharp");
 
             return assemblyReferences.ToArray();
         }
