e220dd2a71ed5b2d8f996be235c5a7f8e39f48fa
Vuk Jovanovic <southpolenator@gmail.com>
2016-05-27 19:55:36 +0200

    Adding WrongCodeTypeException as a start of creating engine exceptions...

---
+++ Source/CsDebugScript.Engine/Exceptions/WrongCodeTypeException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CsDebugScript.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when variable of unexpected code type has been given as an argument.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class WrongCodeTypeException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WrongCodeTypeException" /> class.
+        /// </summary>
+        /// <param name="variable">The variable.</param>
+        /// <param name="argumentName">Name of the argument.</param>
+        /// <param name="expectedText">The expected text.</param>
+        public WrongCodeTypeException(Variable variable, string argumentName, string expectedText)
+            : base(string.Format("Wrong code type [{0}] of passed parameter '{1}'. Expected {2}.", variable.GetCodeType().Name, argumentName, expectedText))
+        {
+            Variable = variable;
+            ArgumentName = argumentName;
+        }
+
+        /// <summary>
+        /// Gets the variable which was passed as an argument.
+        /// </summary>
+        public Variable Variable { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the argument.
+        /// </summary>
+        public string ArgumentName { get; private set; }
+    }
+}
