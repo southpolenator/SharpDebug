5cd144980dcb3b004dd2305e559eabb832ad8136
Vuk Jovanovic <southpolenator@gmail.com>
2016-06-12 00:47:43 +0200

    Adding interactive mode tests...

---
+++ Tests/DbgEngTest/InteractiveModeTests.cs
@@ -0,0 +1,25 @@
+﻿using CsDebugScript;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbgEngTest
+{
+    [TestClass]
+    public class InteractiveModeTests : TestBase
+    {
+        private const string DefaultDumpFile = "NativeDumpTest.x64.dmp";
+        private const string DefaultSymbolPath = @".\";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Initialize(DefaultDumpFile, DefaultSymbolPath);
+        }
+
+        [TestMethod]
+        public void DynamicTest()
+        {
+            Executor.InterpretInteractive("dynamic a = 5");
+            Executor.InterpretInteractive("Console.WriteLine(a);");
+        }
+    }
+}
