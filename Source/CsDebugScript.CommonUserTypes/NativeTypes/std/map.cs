4aac5a092488b76b1c9f1d6506952e6cb5911318
Vuk Jovanovic <southpolenator@gmail.com>
2016-10-17 23:32:44 +0200

    Adding type selector to std::map...

--- Source/CsDebugScript.CommonUserTypes/NativeTypes/std/map.cs
+++ Source/CsDebugScript.CommonUserTypes/NativeTypes/std/map.cs
@@ -1,4 +1,5 @@
 ﻿using CsDebugScript.Exceptions;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -132,19 +133,255 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         }
 
         /// <summary>
-        /// The internal value field inside the std::map
+        /// Microsoft Visual Studio 2015 implementation of std::map
         /// </summary>
-        private UserMember<Variable> value;
+        /// <seealso cref="System.Collections.Generic.IReadOnlyDictionary{TKey, TValue}" />
+        public class VisualStudio2015 : IReadOnlyDictionary<TKey, TValue>
+        {
+            /// <summary>
+            /// The internal value field inside the std::map
+            /// </summary>
+            private UserMember<Variable> value;
+
+            /// <summary>
+            /// The size of the map
+            /// </summary>
+            private UserMember<int> size;
+
+            /// <summary>
+            /// The root of the tree
+            /// </summary>
+            private UserMember<item> root;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="VisualStudio2015"/> class.
+            /// </summary>
+            /// <param name="variable">The variable.</param>
+            public VisualStudio2015(Variable variable)
+            {
+                value = UserMember.Create(() => variable.GetField("_Mypair").GetField("_Myval2").GetField("_Myval2"));
+                size = UserMember.Create(() => (int)Value.GetField("_Mysize"));
+                root = UserMember.Create(() => new item(Value.GetField("_Myhead")).Parent);
+            }
+
+            /// <summary>
+            /// Gets the number of elements in the collection.
+            /// </summary>
+            public int Count
+            {
+                get
+                {
+                    return size.Value;
+                }
+            }
+
+            /// <summary>
+            /// Gets the internal value field inside the std::map
+            /// </summary>
+            private item Root
+            {
+                get
+                {
+                    return root.Value;
+                }
+            }
+
+            /// <summary>
+            /// Gets the internal value field inside the std::map
+            /// </summary>
+            private Variable Value
+            {
+                get
+                {
+                    return value.Value;
+                }
+            }
+
+            /// <summary>
+            /// Gets the <see cref="TValue"/> with the specified key.
+            /// </summary>
+            /// <param name="key">The key to locate.</param>
+            /// <returns>The element that has the specified key in the read-only dictionary.</returns>
+            /// <exception cref="System.Collections.Generic.KeyNotFoundException">The property is retrieved and key is not found.</exception>
+            public TValue this[TKey key]
+            {
+                get
+                {
+                    TValue value;
+
+                    if (!TryGetValue(key, out value))
+                    {
+                        throw new KeyNotFoundException();
+                    }
+
+                    return value;
+                }
+            }
+
+            /// <summary>
+            /// Gets an enumerable collection that contains the keys in the read-only dictionary.
+            /// </summary>
+            public IEnumerable<TKey> Keys
+            {
+                get
+                {
+                    return Enumerate().Select(kvp => kvp.Key);
+                }
+            }
+
+            /// <summary>
+            /// Gets an enumerable collection that contains the values in the read-only dictionary.
+            /// </summary>
+            public IEnumerable<TValue> Values
+            {
+                get
+                {
+                    return Enumerate().Select(kvp => kvp.Value);
+                }
+            }
+
+            /// <summary>
+            /// Determines whether the read-only dictionary contains an element that has the specified key.
+            /// </summary>
+            /// <param name="key">The key to locate.</param>
+            /// <returns>
+            /// true if the read-only dictionary contains an element that has the specified key; otherwise, false.
+            /// </returns>
+            public bool ContainsKey(TKey key)
+            {
+                TValue value;
+
+                return TryGetValue(key, out value);
+            }
+
+            /// <summary>
+            /// Returns an enumerator that iterates through the collection.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.
+            /// </returns>
+            public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            {
+                return Enumerate().GetEnumerator();
+            }
+
+            /// <summary>
+            /// Returns an enumerator that iterates through a collection.
+            /// </summary>
+            /// <returns>
+            /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+            /// </returns>
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return Enumerate().GetEnumerator();
+            }
+
+            /// <summary>
+            /// Gets the value that is associated with the specified key.
+            /// </summary>
+            /// <param name="key">The key to locate.</param>
+            /// <param name="value">When this method returns, the value associated with the specified key, if the key is found; otherwise, the default value for the type of the <paramref name="value" /> parameter. This parameter is passed uninitialized.</param>
+            /// <returns>
+            /// true if the object that implements the <see cref="T:System.Collections.Generic.IReadOnlyDictionary`2" /> interface contains an element that has the specified key; otherwise, false.
+            /// </returns>
+            public bool TryGetValue(TKey key, out TValue value)
+            {
+                // As we don't have comparer (for sorted tree), we will need to scan all items
+                foreach (KeyValuePair<TKey, TValue> kvp in Enumerate())
+                {
+                    if (kvp.Key.Equals(key))
+                    {
+                        value = kvp.Value;
+                        return true;
+                    }
+                }
+
+                value = default(TValue);
+                return false;
+            }
+
+            /// <summary>
+            /// Enumerates this map.
+            /// </summary>
+            private IEnumerable<KeyValuePair<TKey, TValue>> Enumerate()
+            {
+                if (Count > 0 && Root != null)
+                {
+                    Stack<item> items = new Stack<item>();
+
+                    items.Push(Root);
+                    while (items.Count > 0)
+                    {
+                        item item = items.Pop();
+
+                        if (!item.IsNil)
+                        {
+                            yield return new KeyValuePair<TKey, TValue>(item.Key, item.Value);
+                            items.Push(item.Right);
+                            items.Push(item.Left);
+                        }
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Verifies if the specified code type is correct for this class.
+            /// </summary>
+            /// <param name="codeType">The code type.</param>
+            internal static bool VerifyCodeType(CodeType codeType)
+            {
+                // We want to have this kind of hierarchy
+                // _Mypair
+                // | _Myval2
+                //   | _Myval2
+                //     | _Myhead
+                //       | _Parent
+                //       | _Left
+                //       | _Right
+                //       | _Isnil
+                //       | _Myval
+                //         | first
+                //         | second
+                //     | _Mysize
+                CodeType _Mypair, _Myval2, _Myval22, _Myhead, _Parent, _Left, _Right, _Myval, _Mysize, _Isnil, first, second;
+
+                if (!codeType.GetFieldTypes().TryGetValue("_Mypair", out _Mypair))
+                    return false;
+                if (!_Mypair.GetFieldTypes().TryGetValue("_Myval2", out _Myval2))
+                    return false;
+                if (!_Myval2.GetFieldTypes().TryGetValue("_Myval2", out _Myval22))
+                    return false;
+
+                var _Myval2Fields = _Myval22.GetFieldTypes();
+
+                if (!_Myval2Fields.TryGetValue("_Myhead", out _Myhead) || !_Myval2Fields.TryGetValue("_Mysize", out _Mysize))
+                    return false;
+
+                var _MyheadFields = _Myhead.GetFieldTypes();
+
+                if (!_MyheadFields.TryGetValue("_Parent", out _Parent) || !_MyheadFields.TryGetValue("_Left", out _Left) || !_MyheadFields.TryGetValue("_Right", out _Right) || !_MyheadFields.TryGetValue("_Myval", out _Myval) || !_MyheadFields.TryGetValue("_Isnil", out _Isnil))
+                    return false;
+
+                // TODO: When we add std::pair support, use it...
+                if (!_Myval.GetFieldTypes().TryGetValue("first", out first) || !_Myval.GetFieldTypes().TryGetValue("second", out second))
+                    return false;
+
+                return true;
+            }
+        }
 
         /// <summary>
-        /// The size of the map
+        /// The type selector
         /// </summary>
-        private UserMember<int> size;
+        private static TypeSelector<IReadOnlyDictionary<TKey, TValue>> typeSelector = new TypeSelector<IReadOnlyDictionary<TKey, TValue>>(new[]
+        {
+            new Tuple<Type, Func<CodeType, bool>>(typeof(VisualStudio2015), VisualStudio2015.VerifyCodeType),
+        });
 
         /// <summary>
-        /// The root of the tree
+        /// The instance used to read variable data
         /// </summary>
-        private UserMember<item> root;
+        private IReadOnlyDictionary<TKey, TValue> instance;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="map{TKey, TValue}"/> class.
@@ -152,16 +389,33 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         /// <param name="variable">The variable.</param>
         public map(Variable variable)
         {
-            // Verify code type
-            if (!VerifyCodeType(variable.GetCodeType()))
+            instance = typeSelector.SelectType(variable);
+            if (instance == null)
             {
                 throw new WrongCodeTypeException(variable, nameof(variable), "std::map");
             }
+        }
 
-            // Initialize members
-            value = UserMember.Create(() => variable.GetField("_Mypair").GetField("_Myval2").GetField("_Myval2"));
-            size = UserMember.Create(() => (int)Value.GetField("_Mysize"));
-            root = UserMember.Create(() => new item(Value.GetField("_Myhead")).Parent);
+        /// <summary>
+        /// Gets an enumerable collection that contains the keys in the read-only dictionary.
+        /// </summary>
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                return instance.Keys;
+            }
+        }
+
+        /// <summary>
+        /// Gets an enumerable collection that contains the values in the read-only dictionary.
+        /// </summary>
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                return instance.Values;
+            }
         }
 
         /// <summary>
@@ -171,29 +425,7 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         {
             get
             {
-                return size.Value;
-            }
-        }
-
-        /// <summary>
-        /// Gets the internal value field inside the std::map
-        /// </summary>
-        private item Root
-        {
-            get
-            {
-                return root.Value;
-            }
-        }
-
-        /// <summary>
-        /// Gets the internal value field inside the std::map
-        /// </summary>
-        private Variable Value
-        {
-            get
-            {
-                return value.Value;
+                return instance.Count;
             }
         }
 
@@ -207,36 +439,7 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         {
             get
             {
-                TValue value;
-
-                if (!TryGetValue(key, out value))
-                {
-                    throw new KeyNotFoundException();
-                }
-
-                return value;
-            }
-        }
-
-        /// <summary>
-        /// Gets an enumerable collection that contains the keys in the read-only dictionary.
-        /// </summary>
-        public IEnumerable<TKey> Keys
-        {
-            get
-            {
-                return Enumerate().Select(kvp => kvp.Key);
-            }
-        }
-
-        /// <summary>
-        /// Gets an enumerable collection that contains the values in the read-only dictionary.
-        /// </summary>
-        public IEnumerable<TValue> Values
-        {
-            get
-            {
-                return Enumerate().Select(kvp => kvp.Value);
+                return instance[key];
             }
         }
 
@@ -249,31 +452,7 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         /// </returns>
         public bool ContainsKey(TKey key)
         {
-            TValue value;
-
-            return TryGetValue(key, out value);
-        }
-
-        /// <summary>
-        /// Returns an enumerator that iterates through the collection.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.
-        /// </returns>
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            return Enumerate().GetEnumerator();
-        }
-
-        /// <summary>
-        /// Returns an enumerator that iterates through a collection.
-        /// </summary>
-        /// <returns>
-        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
-        /// </returns>
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return Enumerate().GetEnumerator();
+            return instance.ContainsKey(key);
         }
 
         /// <summary>
@@ -286,87 +465,29 @@ namespace CsDebugScript.CommonUserTypes.NativeTypes.std
         /// </returns>
         public bool TryGetValue(TKey key, out TValue value)
         {
-            // As we don't have comparer (for sorted tree), we will need to scan all items
-            foreach (KeyValuePair<TKey, TValue> kvp in Enumerate())
-            {
-                if (kvp.Key.Equals(key))
-                {
-                    value = kvp.Value;
-                    return true;
-                }
-            }
-
-            value = default(TValue);
-            return false;
+            return instance.TryGetValue(key, out value);
         }
 
         /// <summary>
-        /// Enumerates this map.
+        /// Returns an enumerator that iterates through the collection.
         /// </summary>
-        private IEnumerable<KeyValuePair<TKey, TValue>> Enumerate()
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            if (Count > 0 && Root != null)
-            {
-                Stack<item> items = new Stack<item>();
-
-                items.Push(Root);
-                while (items.Count > 0)
-                {
-                    item item = items.Pop();
-
-                    if (!item.IsNil)
-                    {
-                        yield return new KeyValuePair<TKey, TValue>(item.Key, item.Value);
-                        items.Push(item.Right);
-                        items.Push(item.Left);
-                    }
-                }
-            }
+            return instance.GetEnumerator();
         }
 
         /// <summary>
-        /// Verifies if the specified code type is correct for this class.
+        /// Returns an enumerator that iterates through a collection.
         /// </summary>
-        /// <param name="codeType">The code type.</param>
-        private static bool VerifyCodeType(CodeType codeType)
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
         {
-            // We want to have this kind of hierarchy
-            // _Mypair
-            // | _Myval2
-            //   | _Myval2
-            //     | _Myhead
-            //       | _Parent
-            //       | _Left
-            //       | _Right
-            //       | _Isnil
-            //       | _Myval
-            //         | first
-            //         | second
-            //     | _Mysize
-            CodeType _Mypair, _Myval2, _Myval22, _Myhead, _Parent, _Left, _Right, _Myval, _Mysize, _Isnil, first, second;
-
-            if (!codeType.GetFieldTypes().TryGetValue("_Mypair", out _Mypair))
-                return false;
-            if (!_Mypair.GetFieldTypes().TryGetValue("_Myval2", out _Myval2))
-                return false;
-            if (!_Myval2.GetFieldTypes().TryGetValue("_Myval2", out _Myval22))
-                return false;
-
-            var _Myval2Fields = _Myval22.GetFieldTypes();
-
-            if (!_Myval2Fields.TryGetValue("_Myhead", out _Myhead) || !_Myval2Fields.TryGetValue("_Mysize", out _Mysize))
-                return false;
-
-            var _MyheadFields = _Myhead.GetFieldTypes();
-
-            if (!_MyheadFields.TryGetValue("_Parent", out _Parent) || !_MyheadFields.TryGetValue("_Left", out _Left) || !_MyheadFields.TryGetValue("_Right", out _Right) || !_MyheadFields.TryGetValue("_Myval", out _Myval) || !_MyheadFields.TryGetValue("_Isnil", out _Isnil))
-                return false;
-
-            // TODO: When we add std::pair support, use it...
-            if (!_Myval.GetFieldTypes().TryGetValue("first", out first) || !_Myval.GetFieldTypes().TryGetValue("second", out second))
-                return false;
-
-            return true;
+            return instance.GetEnumerator();
         }
     }
 }
