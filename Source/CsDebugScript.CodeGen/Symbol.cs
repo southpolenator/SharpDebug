a02706c86628a559d4f61739261fd27d50491fef
Vuk Jovanovic <southpolenator@gmail.com>
2017-07-16 18:22:26 +0200

    Removing dependency in CodeGen to Engine file (SimpleCache.cs)

--- Source/CsDebugScript.CodeGen/Symbol.cs
+++ Source/CsDebugScript.CodeGen/Symbol.cs
@@ -1,5 +1,4 @@
 ﻿using CsDebugScript.CodeGen.UserTypes;
-using CsDebugScript.Engine.Utility;
 using Dia2Lib;
 using System;
 using System.Collections.Generic;
@@ -324,6 +323,86 @@ namespace CsDebugScript.CodeGen
                 {
                     yield return symbol;
                     unprocessed.AddRange(symbol.BaseClasses);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Helper class for caching results - it is being used as lazy evaluation
+        /// </summary>
+        internal static class SimpleCache
+        {
+            /// <summary>
+            /// Creates a new instance of the <see cref="SimpleCache{T}" /> class.
+            /// </summary>
+            /// <typeparam name="T">Type to be cached</typeparam>
+            /// <param name="populateAction">The function that populates the cache on demand.</param>
+            /// <returns>Simple cache of &lt;T&gt;</returns>
+            public static SimpleCache<T> Create<T>(Func<T> populateAction)
+            {
+                return new SimpleCache<T>(populateAction);
+            }
+        }
+
+        /// <summary>
+        /// Helper class for caching results - it is being used as lazy evaluation
+        /// </summary>
+        /// <typeparam name="T">Type to be cached</typeparam>
+        internal class SimpleCache<T>
+        {
+            /// <summary>
+            /// The populate action
+            /// </summary>
+            private Func<T> populateAction;
+
+            /// <summary>
+            /// The value that is cached
+            /// </summary>
+            private T value;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SimpleCache{T}"/> class.
+            /// </summary>
+            /// <param name="populateAction">The function that populates the cache on demand.</param>
+            public SimpleCache(Func<T> populateAction)
+            {
+                this.populateAction = populateAction;
+            }
+
+            /// <summary>
+            /// Gets a value indicating whether value is cached.
+            /// </summary>
+            /// <value>
+            ///   <c>true</c> if cached; otherwise, <c>false</c>.
+            /// </value>
+            public bool Cached { get; internal set; }
+
+            /// <summary>
+            /// Gets or sets the value. The value will be populated if it wasn't cached.
+            /// </summary>
+            public T Value
+            {
+                get
+                {
+                    if (!Cached)
+                    {
+                        lock (this)
+                        {
+                            if (!Cached)
+                            {
+                                value = populateAction();
+                                Cached = true;
+                            }
+                        }
+                    }
+
+                    return value;
+                }
+
+                set
+                {
+                    this.value = value;
+                    Cached = true;
                 }
             }
         }
