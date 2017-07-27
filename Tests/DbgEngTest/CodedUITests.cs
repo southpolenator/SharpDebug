452af579b2e9966a67dc0b764858a0da4853f00f
Vuk Jovanovic <southpolenator@gmail.com>
2017-07-27 10:02:39 +0200

    Sharing same window between UI tests

--- Tests/DbgEngTest/CodedUITests.cs
+++ Tests/DbgEngTest/CodedUITests.cs
@@ -12,24 +12,55 @@ namespace DbgEngTest
     {
         private const string DefaultDumpFile = NativeDumpTest64.DefaultDumpFile;
         private const string DefaultSymbolPath = NativeDumpTest64.DefaultSymbolPath;
-        private UIMap map;
+        private static UIMap map;
 
         public CodedUITests()
         {
         }
 
+        static CodedUITests()
+        {
+            InitializeDump(DefaultDumpFile, DefaultSymbolPath);
+            Executor.InterpretInteractive("#dbg k");
+        }
+
         [TestInitialize()]
         public void TestInitialize()
         {
-            InitializeDump(DefaultDumpFile, DefaultSymbolPath);
+            OpenInteractiveWindow();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //CloseInteractiveWindow();
+        }
+
+        [TestMethod, Timeout(60000)]
+        [TestCategory("UI")]
+        public void ExecuteDebuggerCommand()
+        {
+            SendInput("{#}dbg{Space}k{Enter}");
+            WaitForExecutionState();
+            WaitForReadyState();
+            UIMap.AssertInteractiveWindowReady();
+        }
+
+        [TestMethod, Timeout(60000)]
+        [TestCategory("UI")]
+        public void MultiLineCode()
+        {
+            SendInput("for {(}int i = 0; i < 5; i{+}{+}{)}{Enter}Console.WriteLine{(}i{)};{Enter}");
+
+            WaitForExecutionState();
+            WaitForReadyState();
+            UIMap.AssertInteractiveWindowReady();
         }
 
         [TestMethod, Timeout(60000)]
         [TestCategory("UI")]
         public void SimpleEndToEnd()
         {
-            OpenInteractiveWindow();
-
             SendInput("var{Space}a{Space}={Space}new{Space}{[}{]}{Space}{RShiftKey}{{}");
             SendInput("{Space}", ModifierKeys.Shift);
             SendInput("1,{Space}2,{Space}3,{Space}4,{Space}5,{Space}6,{Space}7,{Space}{RShiftKey}{}};{Enter}");
@@ -49,37 +80,6 @@ namespace DbgEngTest
             WaitForExecutionState();
             WaitForReadyState();
             UIMap.AssertInteractiveWindowReady();
-
-            CloseInteractiveWindow();
-        }
-
-        [TestMethod, Timeout(30000)]
-        [TestCategory("UI")]
-        public void ExecuteDebuggerCommand()
-        {
-            OpenInteractiveWindow();
-
-            SendInput("{#}dbg{Space}k{Enter}");
-            WaitForExecutionState();
-            WaitForReadyState();
-            UIMap.AssertInteractiveWindowReady();
-
-            CloseInteractiveWindow();
-        }
-
-        [TestMethod, Timeout(60000)]
-        [TestCategory("UI")]
-        public void MultiLineCode()
-        {
-            OpenInteractiveWindow();
-
-            SendInput("for {(}int i = 0; i < 5; i{+}{+}{)}{Enter}Console.WriteLine{(}i{)};{Enter}");
-
-            WaitForExecutionState();
-            WaitForReadyState();
-            UIMap.AssertInteractiveWindowReady();
-
-            CloseInteractiveWindow();
         }
 
         /// <summary>
@@ -98,16 +98,16 @@ namespace DbgEngTest
         }
         private TestContext testContextInstance;
 
-        public UIMap UIMap
+        public static UIMap UIMap
         {
             get
             {
-                if ((this.map == null))
+                if (map == null)
                 {
-                    this.map = new UIMap();
+                    map = new UIMap();
                 }
 
-                return this.map;
+                return map;
             }
         }
 
@@ -119,21 +119,27 @@ namespace DbgEngTest
             }
         }
 
-        private void OpenInteractiveWindow()
+        private static void OpenInteractiveWindow()
         {
-            Executor.ShowInteractiveWindow(false);
+            InteractiveWindow window = UIMap.InteractiveWindow;
 
-            WaitForReadyState(10000);
-            UIMap.AssertInteractiveWindowReady();
+            if (!window.TryFind())
+            {
+                Executor.ShowInteractiveWindow(false);
+
+                window.WaitForControlExist();
+                WaitForReadyState(25000);
+                UIMap.AssertInteractiveWindowReady();
+            }
         }
 
-        private void CloseInteractiveWindow(int millisecondsTimeout = 5000)
+        private static void CloseInteractiveWindow(int millisecondsTimeout = 5000)
         {
             SendInput("q{Space}{Enter}");
             UIMap.InteractiveWindow.WaitForControlNotExist(millisecondsTimeout);
         }
 
-        private void WaitForReadyState(int millisecondsTimeout = 5000)
+        private static void WaitForReadyState(int millisecondsTimeout = 15000)
         {
             PromptLabel label = UIMap.InteractiveWindow.InteractiveWindowContent.ResultContainer.PromptLabel;
             bool happened = label.WaitForControlCondition(c => c.GetProperty(WpfText.PropertyNames.DisplayText).ToString() == "C#> ", millisecondsTimeout);
@@ -147,7 +153,7 @@ namespace DbgEngTest
             Assert.IsTrue(happened);
         }
 
-        private void WaitForExecutionState(int millisecondsTimeout = 5000)
+        private static void WaitForExecutionState(int millisecondsTimeout = 15000)
         {
             PromptLabel label = UIMap.InteractiveWindow.InteractiveWindowContent.ResultContainer.PromptLabel;
             bool happened = label.WaitForControlCondition(c => c.GetProperty(WpfText.PropertyNames.DisplayText).ToString() == "...> ", millisecondsTimeout);
@@ -161,7 +167,7 @@ namespace DbgEngTest
             Assert.IsTrue(happened);
         }
 
-        private void SendInput(string input, ModifierKeys modifierKeys = ModifierKeys.None)
+        private static void SendInput(string input, ModifierKeys modifierKeys = ModifierKeys.None)
         {
             Keyboard.SendKeys(UIMap.InteractiveWindow, input, modifierKeys);
         }
