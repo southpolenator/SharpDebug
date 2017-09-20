dc6666661eba5aaf2b0eca3549c519c7cc2e5c06
Vuk Jovanovic <southpolenator@gmail.com>
2017-09-20 08:04:27 +0200

    Adding top level project that references all others for simplified new project creation using nuget reference.

---
+++ Source/CsDebugScript/DebuggerInitialization.cs
@@ -0,0 +1,35 @@
+﻿using CsDebugScript.DwarfSymbolProvider;
+using CsDebugScript.Engine;
+using CsDebugScript.Engine.Debuggers;
+using DbgEng;
+
+namespace CsDebugScript
+{
+    /// <summary>
+    /// Class for simplified debugger initializion.
+    /// </summary>
+    public static class DebuggerInitialization
+    {
+        /// <summary>
+        /// Initializes debugger for processing the specified dump.
+        /// </summary>
+        /// <param name="dumpPath">Path to the dump.</param>
+        /// <param name="symbolPaths">Array of paths where debugger will look for symbols.</param>
+        public static void OpenDump(string dumpPath, params string[] symbolPaths)
+        {
+            try
+            {
+                IDebugClient debugClient = DebugClient.OpenDumpFile(dumpPath, string.Join(";", symbolPaths));
+                DbgEngDll.InitializeContext(debugClient);
+                Context.InitializeDebugger(Context.Debugger, new DwarfSymbolProvider.DwarfSymbolProvider());
+                Context.ClrProvider = new CLR.ClrMdProvider();
+            }
+            catch
+            {
+                IDebuggerEngine engine = new ElfCoreDumpDebuggingEngine(dumpPath);
+
+                Context.InitializeDebugger(engine, engine.CreateDefaultSymbolProvider());
+            }
+        }
+    }
+}
