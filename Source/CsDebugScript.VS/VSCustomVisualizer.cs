f91d0445c33be4c916498efeccd85d90c5e16e08
Vuk Jovanovic <southpolenator@gmail.com>
2018-05-09 20:52:57 +0200

    Adding custom visualizer stub (empty - not implemented).

---
+++ Source/CsDebugScript.VS/VSCustomVisualizer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.Debugger.ComponentInterfaces;
+using Microsoft.VisualStudio.Debugger.Evaluation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsDebugScript.VS
+{
+    public class VSCustomVisualizer : IDkmCustomVisualizer
+    {
+        public void EvaluateVisualizedExpression(DkmVisualizedExpression visualizedExpression, out DkmEvaluationResult resultObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetChildren(DkmVisualizedExpression visualizedExpression, int initialRequestSize, DkmInspectionContext inspectionContext, out DkmChildVisualizedExpression[] initialChildren, out DkmEvaluationResultEnumContext enumContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetItems(DkmVisualizedExpression visualizedExpression, DkmEvaluationResultEnumContext enumContext, int startIndex, int count, out DkmChildVisualizedExpression[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUnderlyingString(DkmVisualizedExpression visualizedExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValueAsString(DkmVisualizedExpression visualizedExpression, string value, int timeout, out string errorText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UseDefaultEvaluationBehavior(DkmVisualizedExpression visualizedExpression, out bool useDefaultEvaluationBehavior, out DkmEvaluationResult defaultEvaluationResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
