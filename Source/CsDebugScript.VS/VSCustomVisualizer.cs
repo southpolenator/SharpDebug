a47356f26a0ac87ced4d91eed86a1260b1818038
Vuk Jovanovic <southpolenator@gmail.com>
2018-05-10 00:55:14 +0200

    Hello world example of custom visualizer evaluation.

--- Source/CsDebugScript.VS/VSCustomVisualizer.cs
+++ Source/CsDebugScript.VS/VSCustomVisualizer.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.VisualStudio.Debugger.ComponentInterfaces;
+﻿using Microsoft.VisualStudio.Debugger;
+using Microsoft.VisualStudio.Debugger.ComponentInterfaces;
 using Microsoft.VisualStudio.Debugger.Evaluation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsDebugScript.VS
 {
@@ -12,22 +9,64 @@ namespace CsDebugScript.VS
     {
         public void EvaluateVisualizedExpression(DkmVisualizedExpression visualizedExpression, out DkmEvaluationResult resultObject)
         {
-            throw new NotImplementedException();
+            VSCustomVisualizerEvaluator evaluator = new VSCustomVisualizerEvaluator(visualizedExpression);
+
+            resultObject = evaluator.EvaluationResult;
+            visualizedExpression.SetDataItem(DkmDataCreationDisposition.CreateAlways, evaluator);
         }
 
         public void GetChildren(DkmVisualizedExpression visualizedExpression, int initialRequestSize, DkmInspectionContext inspectionContext, out DkmChildVisualizedExpression[] initialChildren, out DkmEvaluationResultEnumContext enumContext)
         {
-            throw new NotImplementedException();
+            VSCustomVisualizerEvaluator evaluator = visualizedExpression.GetDataItem<VSCustomVisualizerEvaluator>();
+
+            // TODO: It would be great to fill in initial children with real data
+            initialChildren = new DkmChildVisualizedExpression[0];
+            enumContext = DkmEvaluationResultEnumContext.Create(1, visualizedExpression.StackFrame, visualizedExpression.InspectionContext, evaluator);
         }
 
         public void GetItems(DkmVisualizedExpression visualizedExpression, DkmEvaluationResultEnumContext enumContext, int startIndex, int count, out DkmChildVisualizedExpression[] items)
         {
-            throw new NotImplementedException();
+            VSCustomVisualizerEvaluator evaluator = visualizedExpression.GetDataItem<VSCustomVisualizerEvaluator>();
+
+            // TODO: Do children evaluation
+            items = new DkmChildVisualizedExpression[count];
+            for (int i = 0; i < count; i++)
+            {
+                DkmSuccessEvaluationResult result = DkmSuccessEvaluationResult.Create(
+                        visualizedExpression.InspectionContext,
+                        visualizedExpression.StackFrame,
+                        "[name]", // Name - Left column
+                        "[fullname]", // FullName - What is being copied when "Add to watch"
+                        DkmEvaluationResultFlags.ReadOnly,
+                        "[value]", // Value - Middle column
+                        "",
+                        "[faketype]", // Type - Right column
+                        DkmEvaluationResultCategory.Property,
+                        DkmEvaluationResultAccessType.None,
+                        DkmEvaluationResultStorageType.None,
+                        DkmEvaluationResultTypeModifierFlags.None,
+                        null,
+                        null,
+                        null,
+                        null);
+
+                items[i] = DkmChildVisualizedExpression.Create(
+                    visualizedExpression.InspectionContext,
+                    visualizedExpression.VisualizerId,
+                    visualizedExpression.SourceId,
+                    visualizedExpression.StackFrame,
+                    visualizedExpression.ValueHome,
+                    result,
+                    visualizedExpression,
+                    (uint)(startIndex + i),
+                    null);
+            }
         }
 
         public string GetUnderlyingString(DkmVisualizedExpression visualizedExpression)
         {
-            throw new NotImplementedException();
+            return "";
+            //throw new NotImplementedException();
         }
 
         public void SetValueAsString(DkmVisualizedExpression visualizedExpression, string value, int timeout, out string errorText)
@@ -37,7 +76,9 @@ namespace CsDebugScript.VS
 
         public void UseDefaultEvaluationBehavior(DkmVisualizedExpression visualizedExpression, out bool useDefaultEvaluationBehavior, out DkmEvaluationResult defaultEvaluationResult)
         {
-            throw new NotImplementedException();
+            // We always want to highjack evaluation, for now :)
+            useDefaultEvaluationBehavior = false;
+            defaultEvaluationResult = null;
         }
     }
 }
---
+++ Source/CsDebugScript.VS/VSCustomVisualizerEvaluator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.Debugger;
+using Microsoft.VisualStudio.Debugger.Evaluation;
+
+namespace CsDebugScript.VS
+{
+    class VSCustomVisualizerEvaluator : DkmDataItem
+    {
+        public VSCustomVisualizerEvaluator(DkmVisualizedExpression visualizedExpression)
+        {
+            VisualizedExpression = visualizedExpression;
+            Evaluate();
+        }
+
+        public DkmVisualizedExpression VisualizedExpression { get; private set; }
+
+        public DkmEvaluationResult EvaluationResult { get; private set; }
+
+        private void Evaluate()
+        {
+            if (VisualizedExpression.TagValue == DkmVisualizedExpression.Tag.RootVisualizedExpression)
+            {
+                DkmRootVisualizedExpression rootVisualizedExpression = VisualizedExpression as DkmRootVisualizedExpression;
+                int processId = rootVisualizedExpression.InspectionSession?.Process?.LivePart?.Id ?? 0;
+                string moduleName = rootVisualizedExpression.Module?.Name; // TODO: This might need to be trimmed of file extension
+                string typeString = rootVisualizedExpression.Type;
+                ulong address = 0;
+                bool hasAddress = false;
+
+                if (VisualizedExpression.ValueHome.TagValue == DkmExpressionValueHome.Tag.PointerValueHome)
+                {
+                    address = (VisualizedExpression.ValueHome as DkmPointerValueHome).Address;
+                    hasAddress = true;
+                }
+
+                if (typeString.Length == 0 || moduleName.Length == 0 || !hasAddress)
+                {
+                    string displayString = "{...CsDebugScript failure...}";
+
+                    EvaluationResult = DkmSuccessEvaluationResult.Create(
+                        VisualizedExpression.InspectionContext,
+                        VisualizedExpression.StackFrame,
+                        rootVisualizedExpression.Name,
+                        rootVisualizedExpression.FullName,
+                        DkmEvaluationResultFlags.ReadOnly,
+                        displayString,
+                        "",
+                        rootVisualizedExpression.Type,
+                        DkmEvaluationResultCategory.Other,
+                        DkmEvaluationResultAccessType.None,
+                        DkmEvaluationResultStorageType.None,
+                        DkmEvaluationResultTypeModifierFlags.None,
+                        null,
+                        null,
+                        null,
+                        null);
+                    return;
+                }
+
+                DkmDataAddress dkmDataAddress = DkmDataAddress.Create(VisualizedExpression.RuntimeInstance, address, rootVisualizedExpression.StackFrame?.InstructionAddress);
+
+                EvaluationResult = DkmSuccessEvaluationResult.Create(
+                    VisualizedExpression.InspectionContext,
+                    VisualizedExpression.StackFrame,
+                    rootVisualizedExpression.Name,
+                    rootVisualizedExpression.FullName,
+                    DkmEvaluationResultFlags.ReadOnly | DkmEvaluationResultFlags.Expandable,
+                    "{...CsDebugScript...}",
+                    "",
+                    rootVisualizedExpression.Type,
+                    DkmEvaluationResultCategory.Other,
+                    DkmEvaluationResultAccessType.None,
+                    DkmEvaluationResultStorageType.None,
+                    DkmEvaluationResultTypeModifierFlags.None,
+                    dkmDataAddress,
+                    null,
+                    null,
+                    null);
+                return;
+            }
+
+            throw new NotImplementedException();
+
+            EvaluationResult = DkmSuccessEvaluationResult.Create(
+                VisualizedExpression.InspectionContext,
+                VisualizedExpression.StackFrame,
+                "Name",
+                "FullName",
+                DkmEvaluationResultFlags.ReadOnly,
+                "{...CsDebugScript child...}",
+                "",
+                "", // Type
+                DkmEvaluationResultCategory.Other,
+                DkmEvaluationResultAccessType.None,
+                DkmEvaluationResultStorageType.None,
+                DkmEvaluationResultTypeModifierFlags.None,
+                null,
+                null,
+                null,
+                null);
+        }
+    }
+}
