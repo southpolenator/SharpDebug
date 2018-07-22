2f5982b2434243ab822a7514d5f500b68096815c
lepiklark <AleksandarTomic88@gmail.com>
2018-07-22 19:24:47 +0200

    Appveyor not to public GITHUB_PAGES_REFERENCES when APPVEYOR_PULL_REQUEST_NUMBER is not set.

--- runtests.bat
+++ runtests.bat
@@ -7,7 +7,7 @@ REM SET TestConsole=C:\Program Files (x86)\Microsoft Visual Studio\2017\Comunity
 REM SET TestConsole=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 SET TestConsole=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 SET TestSettings=%PROJECT_ROOT%\Tests\DbgEngTest\WinDbgCs_%Platform%.testsettings
-SET DbgEngTest=%PROJECT_ROOT%\bin\\Debug\net461\CsDebugScript.Tests.dll
+SET DbgEngTest=%PROJECT_ROOT%\bin\%CONFIGURATION%\net461\CsDebugScript.Tests.dll
 
 "%TestConsole%" /Settings:"%TestSettings%" /inIsolation /Platform:%Platform% "%DbgEngTest%"
 rem "%TestConsole%" /inIsolation /Platform:%Platform% "%DbgEngTest%" /Tests:CsDebugScript.Tests.DebugControlTest.GoBreakContinuosTestDepth,CsDebugScript.Tests.DebugControlTest.BreakpointBreakAndContinue,CsDebugScript.Tests.DebugControlTest.BreakpointSanityTest,CsDebugScript.Tests.DebugControlTest.GoBreakContinousVariablesChange,CsDebugScript.Tests.DebugControlTest.BreakpointWithBreakAfterHit
