dfc98556a7cdf73c8865f2fbd80bf7f2118d929c
southpolenator <southpolenator@gmail.com>
2019-01-06 12:32:48 +0100

    Updating version to 1.4

--- Properties/SharedAssemblyInfo.cs
+++ Properties/SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © WinDbgCs team 2015-2017")]
+[assembly: AssemblyCopyright("Copyright © CsDebugScript team 2015-2019")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -12,5 +12,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3")]
-[assembly: AssemblyFileVersion("1.3")]
+[assembly: AssemblyVersion("1.4")]
+[assembly: AssemblyFileVersion("1.4")]
